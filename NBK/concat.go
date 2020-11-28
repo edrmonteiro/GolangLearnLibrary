b2e44ff32188fdcb79cc609183f2b9d32ac7fc36
Eduardo Monteiro <eduardormonteiro@gmail.com>
2020-11-28 15:32:07 -0300

    Go concat

---
+++ NBK/concat.go
@@ -0,0 +1,21 @@
+/*
+coding: utf-8
+Created on 28/11/2020
+@author: github.com/edrmonteiro
+From: NBK
+Language: Go
+Title: Concat
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	word1 := "Hello"
+	word2 := "world"
+
+	fmt.Println(word1 + " " + word2)
+	fmt.Println(word1, word2)
+}
--- NBK/variables.go
+++ NBK/variables.go
@@ -1,3 +1,12 @@
+/*
+coding: utf-8
+Created on 28/11/2020
+@author: github.com/edrmonteiro
+From: NBK
+Language: Go
+Title: Variables
+
+*/
 package main
 
 import "fmt"
